42fbc18a78071338a3d9c5aa9052e626538fdf5e
GustavH <gkhaavik@gmail.com>
2024-12-01 23:05:57 +0100

    fix: build error

--- src/app/(client)/products/[id]/page.tsx
+++ src/app/(client)/products/[id]/page.tsx
@@ -8,7 +8,7 @@ interface ProductPageProps {
   }
   
   export default async function ProductPage({ params }: ProductPageProps) {
-    const product = await productApi.getProductById(parseInt(params.id));
+    const product = await productApi.getProductById(params.id);
     
     return <ProductDetails product={product} />;
   }
